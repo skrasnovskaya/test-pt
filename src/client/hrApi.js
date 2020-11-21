75231814e2075503970341baf401dcbfcd9ef453
Skras <skraspost@gmail.com>
2020-11-22 01:19:27 +0300

    Fix api endpoint url

--- src/client/hrApi.js
+++ src/client/hrApi.js
@@ -1,10 +1,13 @@
 import { Client } from './client';
 import { Api } from '../utils/api';
 
+/**
+ * FYI: Request goes to https://hr.oat.taocloud.org/v1/ instead of the provided http://hr.oat.taocloud.org/api/ 
+ */
 export class HrApi extends Client {
     constructor() {
         super();
-		this._client = new Api('https://hr.oat.taocloud.org/api/');
+		this._client = new Api('https://hr.oat.taocloud.org/v1/');
     }
 
     async loadData() {
